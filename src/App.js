55a8ccf07accd34e1bee13c131eaedb1a32b90ce
ying <1515886234@qq.com>
2018-11-06 21:50:36 +0800

    ying

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Table, Divider, Tag } from 'antd';
+import 'antd/dist/antd.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      data:[],
+    }
+  }
+  componentDidMount(){
+    this.getData(res=>{
+      this.setState({
+        data:res,
+      })
+    })
+  }
+  getData = (callback) =>{
+    axios.get('http://www.mocky.io/v2/5b766d7b3000005700848af9').then(res=>{
+      console.log(res)
+      if( res.status === 200 ){
+        callback(res.data.apis)
+      }
+    })
+  }
   render() {
+    console.log(this.state.data)
+    const columns = [{
+      title: 'name',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a href="javascript:;">{text}</a>,
+    }, {
+      title: 'description',
+      dataIndex: 'description',
+      key: 'description',
+    }, {
+      title: 'image',
+      dataIndex: 'image',
+      key: 'image',
+    }, {
+      title: 'tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: tags => (
+        <span>
+          {tags.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
+        </span>
+      ),
+    }, {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <span>
+          <a href="javascript:;">Invite {record.name}</a>
+          <Divider type="vertical" />
+          <a href="javascript:;">Delete</a>
+        </span>
+      ),
+    }];
+    
+    const data = this.state.data;
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Table columns={columns} dataSource={data} />
       </div>
     );
   }
