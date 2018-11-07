1a0f10d9155c3933da009e8c3f3802e9686b5615
ying <1515886234@qq.com>
2018-11-07 20:37:16 +0800

    1ying

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
-import { Table, Divider, Tag } from 'antd';
+import { Table, Divider, Tag, Avatar, Modal, Button } from 'antd';
 import 'antd/dist/antd.css';
 
 class App extends Component {
-  constructor(){
+  state = {
+    modal1Visible: false,
+  }
+
+  setModal1Visible(modal1Visible) {
+    this.setState({ modal1Visible });
+  }
+
+  constructor() {
     super();
-    this.state={
-      data:[],
+    this.state = {
+      data: [],
     }
   }
-  componentDidMount(){
-    this.getData(res=>{
+  componentDidMount() {
+    this.getData(res => {
       this.setState({
-        data:res,
+        data: res,
       })
     })
   }
-  getData = (callback) =>{
-    axios.get('http://www.mocky.io/v2/5b766d7b3000005700848af9').then(res=>{
+  getData = (callback) => {
+    axios.get('http://www.mocky.io/v2/5b766d7b3000005700848af9').then(res => {
       console.log(res)
-      if( res.status === 200 ){
+      if (res.status === 200) {
         callback(res.data.apis)
       }
     })
@@ -32,15 +40,26 @@ class App extends Component {
       title: 'name',
       dataIndex: 'name',
       key: 'name',
-      render: text => <a href="javascript:;">{text}</a>,
     }, {
       title: 'description',
       dataIndex: 'description',
       key: 'description',
+      render: text => <Divider title={text} type="vertical" style={{ width: 300, whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', height: 20, cursor: 'pointer' }}>{text}</Divider>,
     }, {
       title: 'image',
       dataIndex: 'image',
       key: 'image',
+      render: image => <img alt="example" src={image} style={{ margin: '10px' }} />
+    }, {
+      title: 'humanURL',
+      dataIndex: 'humanURL',
+      key: 'humanURL',
+      render: humanURL => <a href={humanURL}>{humanURL}</a>,
+    }, {
+      title: 'baseURL',
+      dataIndex: 'baseURL',
+      key: 'baseURL',
+      render: baseURL => <a href={baseURL}>{baseURL}</a>,
     }, {
       title: 'tags',
       key: 'tags',
@@ -51,22 +70,35 @@ class App extends Component {
         </span>
       ),
     }, {
-      title: 'Action',
-      key: 'action',
-      render: (text, record) => (
-        <span>
-          <a href="javascript:;">Invite {record.name}</a>
-          <Divider type="vertical" />
-          <a href="javascript:;">Delete</a>
-        </span>
+      title: 'properties',
+      key: 'properties',
+      dataIndex: 'properties',
+      render: properties => (
+        <div>
+          <Button key={properties} onClick={() => this.setModal1Visible(true)}>properties</Button>
+          <Modal
+            title='properties'
+            visible={this.state.modal1Visible}
+            onOk={() => this.setModal1Visible(false)}
+            onCancel={() => this.setModal1Visible(false)}
+          >
+            {properties.map(properties =>
+              <div>
+                <p>{properties.type}</p>
+                <a href={properties.url}>{properties.url}</a>
+              </div>)}
+          </Modal>
+        </div>
       ),
     }];
-    
+
     const data = this.state.data;
-    
+
+    const fileList = this.state.data.image;
+
     return (
       <div className="App">
-        <Table columns={columns} dataSource={data} />
+        <Table rowKey={record => record.id} columns={columns} dataSource={data} />
       </div>
     );
   }
